48d9b959eca5321448df6d15f04d118d2aa82404
Marty Mars <mars@MarsMac.local>
2018-05-21 22:45:27 -0700

    Text_Helper Fix

--- app/models/person_pet_experiment.rb
+++
@@ -1,5 +0,0 @@
-class PersonPetExperiment < ApplicationRecord
-    validates :height, presence: true
-    validates :weight, presence: true
-    attr_accessor :pet_kind
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'    
+  get '/welcome', to: 'welcome#index'
   get '/ppe', to: 'person_pet_experiment#ppe'
   post "ppe", to: "person_pet_experiment#create"
   get '/show', to: 'person_pet_experiment#show'
