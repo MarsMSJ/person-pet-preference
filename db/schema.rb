9f3e125b4d560a4ac118372e24f3143a0b809bf7
Marty Mars <mars@MarsMac.local>
2018-05-22 15:19:10 -0700

    Db Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_032350) do
+ActiveRecord::Schema.define(version: 2018_05_22_221700) do
 
   create_table "person_pet_experiments", force: :cascade do |t|
     t.decimal "height"
---
+++ test/models/person_pet_experiment_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PersonPetExperimentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
